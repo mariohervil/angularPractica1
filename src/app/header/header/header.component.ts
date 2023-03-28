c92b0c23f39c46e9fffb4b533e5e9b3fc19a4822
mariohervil <mariohervil@campus.monlau.com>
2023-03-28 17:29:59 +0200

    A

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,18 +1,28 @@
-import {Component} from '@angular/core';
-import {LightModeService} from '../../lightMode/light-mode.service'
+import { Component } from '@angular/core';
+import { LightModeService } from '../../lightMode/light-mode.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  buttonColors: { [key: string]: string } = {
+    red: 'white',
+    blue: 'white',
+    green: 'white',
+  };
 
-  constructor(public lightMode: LightModeService) {
-  }
+  constructor(public lightMode: LightModeService) {}
 
   toggleLightMode() {
     this.lightMode.toggleLightMode();
-    console.log(this.lightMode)
+    console.log(this.lightMode);
+  }
+
+  changeColor(color: string) {
+    if (this.buttonColors[color] === color) {
+      this.buttonColors[color] = 'white';
+    } else this.buttonColors[color] = color;
   }
 }
